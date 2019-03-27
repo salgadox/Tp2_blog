02d45d53a3a49e3b3e8f0e8ebcbc10cc355fe6c4
Xim <xsal@protonmail.com>
2019-03-27 17:45:56 +0100

    modify home: 3 articles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
    
     public function index()
     { 
-        $posts = Post::all();
+        $posts = Post::orderBy('post_date','desc')->take(3)->get();
 
     	return view('welcome', array('posts'=>$posts));
     }
